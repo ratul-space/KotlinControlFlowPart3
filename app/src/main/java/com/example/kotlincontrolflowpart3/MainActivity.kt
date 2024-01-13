47aafa654eed8d997f13eafdd8edbbe58c80fce9
ratul-space <to.ratul.hosen@gmail.com>
2024-01-13 22:59:50 +0600

    feat: add tag

--- app/src/main/java/com/example/kotlincontrolflowpart3/MainActivity.kt
+++ app/src/main/java/com/example/kotlincontrolflowpart3/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = javaClass.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
