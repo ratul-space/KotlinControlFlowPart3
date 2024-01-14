3800d2c8b91afd9cc2586319b8f3bb16a4e41fb7
ratul-space <to.ratul.hosen@gmail.com>
2024-01-14 08:44:31 +0600

    feat: uses of for loop with breaks

--- app/src/main/java/com/example/kotlincontrolflowpart3/MainActivity.kt
+++ app/src/main/java/com/example/kotlincontrolflowpart3/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.kotlincontrolflowpart3
 
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = javaClass.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (i in 1..7) {
+            if (i == 3) {
+                break
+            }
+            Log.d(TAG, "i =" + i)
+
+        }
+
+
+        Log.d(TAG, "onCreate: ends")
     }
 }
